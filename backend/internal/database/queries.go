b4ca69aee7628d533d580b6190026877023ec8fc
Benoit.G <benoitgaugiran@gmail.com>
2025-03-12 23:22:23 +0100

    Test add user

--- backend/internal/database/queries.go
+++ backend/internal/database/queries.go
@@ -1,8 +1,27 @@
 package database
 
-// func AddUser() error {
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+)
 
-// }
+func AddUser(parentsContext context.Context, bdd string, id int, username, password string) error {
+	ctx, cancel := context.WithTimeout(parentsContext, 2*time.Second)
+	defer cancel()
+
+	query := fmt.Sprintf("INSERT INTO %s (username, password) VALUES ($1, $2)", bdd)
+
+	_, err := DBPool.Exec(ctx, query, username, password)
+	if err != nil {
+		log.Println("Error inserting user:", err)
+		return err
+	}
+
+	log.Println("✅ User added successfully:", username)
+	return nil
+}
 
 func DeleteUser() {
 
