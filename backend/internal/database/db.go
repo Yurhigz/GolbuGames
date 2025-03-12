22dc22cb051951f038e530137853c1211d3293fb
Benoit.G <benoitgaugiran@gmail.com>
2025-03-12 23:22:13 +0100

    Update

--- backend/internal/database/db.go
+++ backend/internal/database/db.go
@@ -20,22 +20,24 @@ func InitDB() error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Configurer et créer le pool de connexions
+	// Configuration du pool à partir de la dburl
 	poolConfig, err := pgxpool.ParseConfig(dbURL)
 	if err != nil {
 		return fmt.Errorf("failed to parse DB config: %w", err)
 	}
 
-	poolConfig.MaxConns = 20               // Maximum 20 connexions actives
-	poolConfig.MinConns = 5                // Minimum 5 connexions actives
-	poolConfig.MaxConnLifetime = time.Hour // Ferme une connexion après 1h
+	poolConfig.MaxConns = 20                      // Maximum 20 connexions actives
+	poolConfig.MinConns = 5                       // Minimum 5 connexions actives
+	poolConfig.MaxConnLifetime = time.Hour        // Ferme une connexion après 1h
+	poolConfig.MaxConnIdleTime = 10 * time.Minute // limite du temps d'inactivité pour un pool
 
+	//  création des pools à l'aide des éléments de configuration précédents
 	DBPool, err = pgxpool.NewWithConfig(ctx, poolConfig)
 	if err != nil {
 		return fmt.Errorf("unable to create connection pool: %w", err)
 	}
 
-	// Vérifier la connexion
+	// Vérifier la connexion avec la BDD
 	err = DBPool.Ping(ctx)
 	if err != nil {
 		return fmt.Errorf("cannot reach database: %w", err)
@@ -55,3 +57,5 @@ func CloseDB() {
 
 // os.Getenv("DATABASE_URL")
 //  https://donchev.is/post/working-with-postgresql-in-go-using-pgx/
+//  Il faudra mettre en place une gestion des connexions dans le cas de figure où la DB redémarre avec une boucle de tentative de reconnexion
+// et éventuellement limiter le nombre de tentatives ou mettre un temps entre chaque tentative
