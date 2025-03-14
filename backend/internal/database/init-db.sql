36b58efc5b790803462b5ff833eb40877284d45b
Benoit.G <benoitgaugiran@gmail.com>
2025-03-14 11:33:43 +0100

    Update architecture

--- backend/internal/database/init-db.sql
+++ backend/internal/database/init-db.sql
@@ -1,17 +1,9 @@
 -- init-db.sql
-CREATE DATABASE sudokudb;
-CREATE DATABASE membersgg;
+CREATE DATABASE golbugamesdb;
 
-\c sudokudb
+\c golbugamesdb
 
-CREATE TABLE sudoku (
-    id SERIAL PRIMARY KEY,
-    board VARCHAR(81) NOT NULL,
-    solution VARCHAR(81) NOT NULL,
-    difficulty VARCHAR(10) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
+-- Authentification générale peu importe le jeu
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE,
@@ -19,22 +11,30 @@ CREATE TABLE users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Gestion des sessions peu importe le jeu 
 CREATE TABLE sessions (
     id SERIAL PRIMARY KEY,
+    game VARCHAR(100) NOT NULL,
     user_id INT NOT NULL,
     token VARCHAR(100) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE -- Suppression en cascade en cas de suppresion d'un utilisateur, mécanisme à confirmer
 );
 
-CREATE TABLE scores (
+
+CREATE TABLE sudoku_games (
+    id SERIAL PRIMARY KEY,
+    board VARCHAR(81) NOT NULL,
+    solution VARCHAR(81) NOT NULL,
+    difficulty VARCHAR(10) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE games_scores (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
+    game_type VARCHAR(100) NOT NULL,
     score INT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
 );
-
-\c membersgg 
-
--- Ajouter les tables après s'est connecté à la base de données membersgg
