ab33387c80cc4198add77b718fb28383c4c6ce68
Benoit.G <benoitgaugiran@gmail.com>
2025-03-14 11:33:09 +0100

    test Add / Delete unitaires

---
+++ backend/tests/unit_tests/tests.go
@@ -0,0 +1,25 @@
+package unit_tests
+
+import (
+	"context"
+	"golbugames/backend/internal/database"
+	"log"
+)
+
+func CreateUserTest(ctx context.Context) error {
+	errTest := database.AddUser(ctx, "test", "test123")
+	if errTest != nil {
+		log.Println("Error when using function <database.AddUser>", errTest)
+		return errTest
+	}
+	return nil
+}
+
+func DeleteUserTest(ctx context.Context) error {
+	errTest := database.DeleteUser(ctx, 1)
+	if errTest != nil {
+		log.Println("Error when using function <database.AddUser>", errTest)
+		return errTest
+	}
+	return nil
+}
