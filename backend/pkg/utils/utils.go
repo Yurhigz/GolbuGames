8572dde29733652a4dbc7dcec239422754069dcf
Benoit.G <benoitgaugiran@gmail.com>
2025-03-16 14:30:59 +0100

    GridTransformer added

--- backend/pkg/utils/utils.go
+++ backend/pkg/utils/utils.go
@@ -1,6 +1,10 @@
 package utils
 
-import "math/rand/v2"
+import (
+	"golbugames/backend/pkg/types"
+	"math/rand/v2"
+	"strconv"
+)
 
 func RandRange(min, max int) int {
 	return rand.IntN(max-min) + min
@@ -9,3 +13,17 @@ func RandRange(min, max int) int {
 // Fonction de hashage pour les passwords
 
 // Fonction de transformation des données de type sudokuGrid en mode string
+
+func GridTransformer(sudokuGrid *types.MainGrid) string {
+	var FlattenedSudoku string
+	for _, row := range sudokuGrid {
+		for i, col := range row {
+			if i < len(row)-1 {
+				FlattenedSudoku += strconv.Itoa(col) + ","
+			} else {
+				FlattenedSudoku += strconv.Itoa(col) + ";"
+			}
+		}
+	}
+	return FlattenedSudoku
+}
