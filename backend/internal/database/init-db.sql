8ed18b7f9ce63ddeaa1324ff2821840afd8aa114
Benoit.G <benoitgaugiran@gmail.com>
2025-04-10 23:21:55 +0200

    Tables correction + added new tables

--- backend/internal/database/init-db.sql
+++ backend/internal/database/init-db.sql
@@ -34,8 +34,33 @@ CREATE TABLE sudoku_games (
 CREATE TABLE games_scores (
     id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    game_type VARCHAR(100) NOT NULL,
-    score INT NOT NULL,
+    opponent_id INT, -- optionnel selon le mode de jeu
+    game_mode VARCHAR(20) NOT NULL CHECK (game_mode IN ('solo', '1v1')),
+    results INT , -- 0 player 1 won, 1 even , 2 player 2 won
+    completion_time INT,  -- temps en secondes lors des 1v1
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE 
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    FOREIGN KEY (opponent_id) REFERENCES users(id) ON DELETE SET NULL
 );
+
+CREATE TABLE user_stats (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    total_games INT DEFAULT 0,
+    total_wins INT DEFAULT 0,
+    total_losses INT DEFAULT 0,
+    total_draws INT DEFAULT 0,
+    total_time INT DEFAULT 0, -- temps total de jeu en secondes
+    average_time INT DEFAULT 0, -- temps moyen de jeu en secondes
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
+
+CREATE TABLE leaderboard (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    rank INT NOT NULL,
+    elo_score INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+)
