bd6cffa0f8962f192cee981e44dc0743b83760fd
Benoit.G <benoitgaugiran@gmail.com>
2025-04-12 00:10:27 +0200

    Update des routes

--- backend/internal/api/router/h_routers.go
+++ backend/internal/api/router/h_routers.go
@@ -6,16 +6,23 @@ import (
 )
 
 func InitRoutesSudoku(mux *http.ServeMux) {
+	// Users API
 	mux.HandleFunc("POST /create_user", handlers.CreateUser)
-	mux.HandleFunc("DELETE /delete_user", handlers.DeleteUser)
+	mux.HandleFunc("DELETE /delete_user/{id}", handlers.DeleteUser)
 	mux.HandleFunc("GET /user/{id}", handlers.GetUser)
+	mux.HandleFunc("POST /updateuser/{id}", handlers.UpdateUserPassword)
+	mux.HandleFunc("GET /user_stats/{id}", handlers.GetUserStats)
+
+	// Sudoku API
 	mux.HandleFunc("POST /add_grid", handlers.AddGrid)
-	mux.HandleFunc("GET /random_grid", handlers.GetGrid)
-	mux.HandleFunc("POST /submit_game", handlers.SubmitSoloGame)
-	mux.HandleFunc("GET /user_stats", handlers.GetUserStats)
-	mux.HandleFunc("GET /leaderboard", handlers.GetLeaderboard)
-	mux.HandleFunc("POST /updateuser", handlers.UpdateUser)
-	mux.HandleFunc("GET /user_history", handlers.GetUserHistory)
-	mux.HandleFunc("POST /save_game", handlers.SaveGameProgress)
+	mux.HandleFunc("GET /grid", handlers.GetGrid)
+
+	// Game API
+	mux.HandleFunc("POST /submit_solo_game", handlers.SubmitSoloGame)
+	mux.HandleFunc("POST /submit_multi_game", handlers.SubmitMultiGame)
+
+	// mux.HandleFunc("GET /leaderboard", handlers.GetLeaderboard)
+	// mux.HandleFunc("GET /user_history", handlers.GetUserHistory)
+	// mux.HandleFunc("POST /save_game", handlers.SaveGameProgress)
 
 }
