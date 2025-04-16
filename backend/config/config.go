b891db588a230dc7f21b82e738eba4e86acb5ff3
Benoit.G <benoitgaugiran@gmail.com>
2025-04-16 23:29:02 +0200

    Standby grid generation

--- backend/config/config.go
+++ backend/config/config.go
@@ -1,35 +1,29 @@
 package config
 
-import (
-	"context"
-	"golbugames/backend/internal/games/sudoku"
-	"golbugames/backend/pkg/utils"
-)
-
 // Ajouter une initialisation de la création des grilles complètes
 // Envisager de mettre en place un système de contrôle pour générer des grilles selon le volume réalisé ou le nombre d'utilisateurs
 
-func GenerateAndStoreGrid(ctx context.Context, difficulty string) error {
-	grid, err := sudoku.GenerateSolvedGrid()
-	if err != nil {
-		return err
-	}
+// func GenerateAndStoreGrid(ctx context.Context, difficulty string) error {
+// 	grid, err := sudoku.GenerateSolvedGrid()
+// 	if err != nil {
+// 		return err
+// 	}
 
-	var board, solution string
+// 	var board, solution string
 
-	solution = utils.GridTransformer(grid)
+// 	solution = utils.GridTransformer(grid)
 
-	boardGrid, err := sudoku.GeneratePlayableGrid(grid, difficulty)
-	if err != nil {
-		return err
-	}
+// 	boardGrid, err := sudoku.GeneratePlayableGrid(grid, difficulty)
+// 	if err != nil {
+// 		return err
+// 	}
 
-	board = utils.GridTransformer(boardGrid)
+// 	board = utils.GridTransformer(boardGrid)
 
-	err = sudoku.AddGrid(ctx, board, solution, difficulty)
+// 	err = sudoku.AddGrid(ctx, board, solution, difficulty)
 
-	if err != nil {
-		return err
-	}
-	return nil
-}
+// 	if err != nil {
+// 		return err
+// 	}
+// 	return nil
+// }
