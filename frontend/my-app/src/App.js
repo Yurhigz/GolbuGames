333e71220da00859884ffc862c3958df5126fdf1
Benoit.G <benoitgaugiran@gmail.com>
2025-05-07 23:33:41 +0200

    WIP sudoku

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,25 +1,103 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function Square() {
+  return (<div className="sudoku-board">
+  <div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+<div className="board-row">
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+  <button className="square"></button>
+</div>
+</div>)
 }
-
-export default App;
