c9cb7fa4efda402c26a6e9765d4ce9584f59cbe1
Benoit.G <benoitgaugiran@gmail.com>
2025-05-08 23:02:28 +0200

    Footer

---
+++ frontend/my-app/src/pages/legal/Terms.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Terms = () => {
+    return (
+        <div>
+            <h2>Conditions d'utilisation</h2>
+            <p>En utilisant GolbuGames, vous acceptez les conditions suivantes...</p>
+        </div>
+    );
+};
+
+export default Terms;
