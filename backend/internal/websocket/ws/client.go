2f9012a6244f3c3cdab7cc94e74cc8eb60cb6f25
Benoit.G <benoitgaugiran@gmail.com>
2025-06-10 23:09:54 +0200

    Quick first steps

--- backend/internal/websocket/ws/client.go
+++ backend/internal/websocket/ws/client.go
@@ -1,13 +1,31 @@
 package ws
 
-import "net"
+import (
+	"net"
+	"sync"
+)
 
 type Client struct {
 	clientId string
 	Conn     net.Conn
+	mu       sync.Mutex
+	send     chan []byte
+	hub      *Hub
 }
 
 // Représente une connexion WebSocket
 // Gère la lecture/écriture des messages
 // Maintient la connexion active ping récurrent
 // Gère la déconnexion
+
+func newClient() *Client {
+	return &Client{}
+}
+
+func (c *Client) writePump() {
+
+}
+
+func (c *Client) readPump() {
+
+}
