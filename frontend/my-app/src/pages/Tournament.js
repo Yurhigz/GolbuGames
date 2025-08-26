0ad87db218ec24e540d5ccdb69154fd455a190ad
Stanislas Bertreux <bertreux.stanislas@gmail.com>
2025-08-27 01:42:33 +0200

    Début correction style ajout ami

--- frontend/my-app/src/pages/Tournament.js
+++ frontend/my-app/src/pages/Tournament.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Input } from "../components/Input";
 import { Card } from "../components/Card";
 import "./Tournament.css";
