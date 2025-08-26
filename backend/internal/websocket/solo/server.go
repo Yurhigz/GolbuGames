45e0fed81daf3ec7dbef4f800552f9d49c8cf7d4
Benoit.G <benoitgaugiran@gmail.com>
2025-08-26 22:29:24 +0200

    Update websocket handler

--- backend/internal/websocket/solo/server.go
+++ backend/internal/websocket/solo/server.go
@@ -2,6 +2,7 @@ package solo
 
 import (
 	"fmt"
+	"golbugames/backend/internal/sudoku/repository"
 	"golbugames/backend/internal/websocket"
 	"log"
 	"net/http"
@@ -9,6 +10,12 @@ import (
 
 // WebsocketHandler Solo
 func WebsocketHandlerSolo(w http.ResponseWriter, r *http.Request) {
+	difficulty := r.URL.Query().Get("difficulty")
+
+	if difficulty == "" {
+		difficulty = "easy"
+	}
+
 	conn, err := websocket.UpgradeConnection(w, r)
 	if err != nil {
 		http.Error(w, fmt.Sprintf("Erreur d'upgrade WebSocket: %v", err), http.StatusInternalServerError)
@@ -18,11 +25,30 @@ func WebsocketHandlerSolo(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("[INFO] WebSocket connection established!")
 
+	sudokuGrid, err := repository.GetRandomGridDB(r.Context(), difficulty)
+	if err != nil {
+		log.Printf("[ERR] Impossible to retrieve the grid: %v", err)
+		conn.Close()
+		return
+	}
+
 	client := newSoloClient(conn)
+	client.playable = sudokuGrid.Board
+	client.solution = sudokuGrid.Solution
+	log.Printf("[INFO] New client created")
 
 	go client.writePump()
 	go client.readPump()
 
-	log.Printf("[INFO] Nouveau client crée")
+	grid := &websocket.Frame{
+		Opcode: websocket.OpcodeText,
+		FIN:    true,
+		Payload: []byte(fmt.Sprintf(
+			`{"type":"init","grid":"%s","difficulty":"%s"}`,
+			client.playable, difficulty,
+		)),
+	}
+
+	client.send <- grid
 
 }
