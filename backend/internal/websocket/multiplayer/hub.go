014805aa9694c5f6bdf2a227ad1452049977c454
Benoit.G <benoitgaugiran@gmail.com>
2025-09-19 15:45:24 +0200

    Features: Matchmaking and utils functions

--- backend/internal/websocket/multiplayer/hub.go
+++ backend/internal/websocket/multiplayer/hub.go
@@ -3,10 +3,12 @@ package multiplayer
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"golbugames/backend/internal/sudoku/repository"
 	"golbugames/backend/internal/websocket"
 	"golbugames/backend/internal/websocket/protocol"
 	"sync"
+	"time"
 )
 
 // Le fonctionnement avec un système de hubmanager va permettre de créer des rooms de communication.
@@ -21,6 +23,7 @@ type HubManager struct {
 }
 
 type Hub struct {
+	gameState  int
 	clients    [2]*Client
 	register   chan *Client
 	unregister chan *Client
@@ -65,6 +68,15 @@ func (hm *HubManager) CreateHub(ctx context.Context, matchId string) (*Hub, erro
 
 func (hm *HubManager) GetHub(matchId string) *Hub {
 	return hm.hubs[matchId]
+}
+
+func (hm *HubManager) RemoveHub(matchId string) {
+	if hub, exists := hm.hubs[matchId]; exists {
+		close(hub.register)
+		close(hub.unregister)
+		close(hub.broadcast)
+		delete(hm.hubs, matchId)
+	}
 }
 
 func (h *Hub) run() {
@@ -132,4 +144,89 @@ func (h *Hub) run() {
 			}
 		}
 	}
+}
+
+func (hm *HubManager) RemoveClientFromQueue(client *Client) {
+	hm.mu.Lock()
+	defer hm.mu.Unlock()
+	for i, c := range hm.ClientQueue {
+		if c == client {
+			hm.ClientQueue = append(hm.ClientQueue[:i], hm.ClientQueue[i+1:]...)
+			return
+		}
+	}
+}
+
+func (h *Hub) clientCount() int {
+	count := 0
+	for _, c := range h.clients {
+		if c != nil {
+			count++
+		}
+	}
+	return count
+}
+
+func createId() string {
+	return fmt.Sprintf("hub_%d", time.Now().UnixNano())
+}
+
+func (hm *HubManager) MatchmakingLoop() {
+	for {
+		time.Sleep(5 * time.Second) // Ajuster la fréquence de vérification si nécessaire
+		hm.mu.Lock()
+		queue := make([]*Client, len(hm.ClientQueue))
+		copy(queue, hm.ClientQueue)
+		hm.mu.Unlock()
+
+		if len(hm.hubs) > 0 && len(queue) > 0 {
+			fmt.Printf("Current matchmaking queue length: %d\n", len(queue))
+			fmt.Printf("Current hubs count: %d\n", len(hm.hubs))
+			availability := false
+			var availableHub *Hub
+			for _, hub := range hm.hubs {
+				if hub.gameState == gameWaiting && hub.clientCount() < 2 {
+					availability = true
+					availableHub = hub
+					break
+				}
+			}
+			if !availability {
+				fmt.Printf("No available hub found, creating a new one\n")
+				availableHub = hm.CreateHub(createId())
+			}
+
+			for _, client := range hm.ClientQueue {
+				if client.matchId == "" {
+					availableHub.register <- client
+					hm.RemoveClientFromQueue(client)
+					client.send <- NewTextFrame("You have been matched with an opponent!")
+					fmt.Printf("A match has been made\n")
+
+				}
+				if availableHub.clientCount() == 2 {
+					fmt.Printf("Hub %s is now full with 2 clients\n", availableHub.hubId)
+					availableHub.gameState = gamesOngoing
+					break
+				}
+			}
+
+		} else if len(queue) >= 1 {
+			var longestWaitingTime *Client
+			for _, client := range queue {
+				waiting_time := time.Since(client.queueTime)
+				if longestWaitingTime == nil || waiting_time > time.Since(longestWaitingTime.queueTime) {
+					longestWaitingTime = client
+				}
+			}
+			if longestWaitingTime != nil {
+				hub := hm.CreateHub(createId())
+				time.Sleep(1000 * time.Millisecond)
+				hub.register <- longestWaitingTime
+				hm.RemoveClientFromQueue(longestWaitingTime)
+			}
+
+		}
+	}
+
 }
