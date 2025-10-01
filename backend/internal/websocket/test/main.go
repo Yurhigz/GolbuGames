4d6872c2986a57e027810fea9b08c7aedcd5e16b
Pozymandiaz <lucasverstraete01@gmail.com>
2025-10-01 14:55:28 +0200

    maj front pour le mutli + initdb et grid

--- backend/internal/websocket/test/main.go
+++ backend/internal/websocket/test/main.go
@@ -3,17 +3,33 @@ package main
 import (
 	"context"
 	"fmt"
+	"golbugames/backend/config"
+	"golbugames/backend/internal/database"
 	"golbugames/backend/internal/websocket/multiplayer"
+	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	ctx := context.Background()
 	parentContext := context.Background()
 	HubManager := multiplayer.NewHubManager()
 	go HubManager.MatchmakingLoop(parentContext)
 
 	// A debug
+	// Init DB
+	err := database.InitDB(ctx)
+	if err != nil {
+		log.Fatalf("Failed to initialize database: %v", err)
+	}
+
+	// Init Sudoku Grids
+
+	err = config.InitGridGeneration(ctx)
+	if err != nil {
+		log.Fatalf("Failed to generate Grids : %v", err)
+	}
 	// go HubManager.HubCleanupLoop()
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		multiplayer.WebsocketHandler(w, r, HubManager)
